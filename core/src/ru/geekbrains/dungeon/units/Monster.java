2a071c5070b71eb17ff5c46d1ca70d472970d443
Skopin <Skopin_MI@mail.ru>
2020-11-21 08:44:57 +0300

    // Домашнее задания:
        // 1. Разобраться с кодом;
        // 2. Монстры охотятся за героем, только если он находится в радиусе N клеток (пусть 5),
        // в противном случае, бегают на случайную клетку

--- core/src/ru/geekbrains/dungeon/units/Monster.java
+++ core/src/ru/geekbrains/dungeon/units/Monster.java
@@ -10,13 +10,14 @@ public class Monster extends Unit {
     private Unit target;
 
 //    дальность зрения
-    
+    private int vigilance;
 
     public Monster(TextureAtlas atlas, GameController gc) {
         super(gc, 5, 2, 10);
         this.texture = atlas.findRegion("monster");
         this.textureHp = atlas.findRegion("hp");
         this.hp = -1;
+        this.vigilance = 5;
     }
 
     public void activate(int cellX, int cellY) {
@@ -48,19 +49,50 @@ public class Monster extends Unit {
 
     public void tryToMove() {
         int bestX = -1, bestY = -1;
-        float bestDst = 10000;
-        for (int i = cellX - 1; i <= cellX + 1; i++) {
-            for (int j = cellY - 1; j <= cellY + 1; j++) {
-                if (Math.abs(cellX - i) + Math.abs(cellY - j) == 1 && gc.getGameMap().isCellPassable(i, j) && gc.getUnitController().isCellFree(i, j)) {
-                    float dst = (float) Math.sqrt((i - target.getCellX()) * (i - target.getCellX()) + (j - target.getCellY()) * (j - target.getCellY()));
-                    if (dst < bestDst) {
-                        bestDst = dst;
-                        bestX = i;
-                        bestY = j;
+        // как меняются bestX bestY
+        // если враг не в поле видимости :
+        if (!searchForEnemy()) {
+            // рандомно меняется либо Х либо У
+            if (Math.random() > 0.5) {
+                bestX = cellX + howChange();
+                bestY = cellY;
+            } else {
+                bestX = cellX;
+                bestY = cellY + howChange();
+            }
+        } else {
+            float bestDst = 10000;
+            for (int i = cellX - 1; i <= cellX + 1; i++) {
+                for (int j = cellY - 1; j <= cellY + 1; j++) {
+                    if (Math.abs(cellX - i) + Math.abs(cellY - j) == 1 && gc.getGameMap().isCellPassable(i, j) && gc.getUnitController().isCellFree(i, j)) {
+                        float dst = (float) Math.sqrt((i - target.getCellX()) * (i - target.getCellX()) + (j - target.getCellY()) * (j - target.getCellY()));
+                        if (dst < bestDst) {
+                            bestDst = dst;
+                            bestX = i;
+                            bestY = j;
+                        }
                     }
                 }
             }
         }
         goTo(bestX, bestY);
+    }
+// есть ли враг в поле видимости?
+    public boolean searchForEnemy () {
+        float dst = (float) Math.sqrt((cellX - target.getCellX()) * (cellX - target.getCellX()) +
+                (cellY - target.getCellY()) * (cellY - target.getCellY()));
+        System.out.println(dst);
+        if (dst < vigilance) {
+            return true;
+        }
+        return false;
+    }
+// рандомное +1 или -1
+    public int howChange () {
+        if (Math.random() > 0.5) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
